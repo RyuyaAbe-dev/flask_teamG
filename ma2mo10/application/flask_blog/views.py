249d13396ad663c71ba108313a00e73b173e49d6
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-06-01 04:42:15 +0000

    Update views.py by ma2mo10

--- ma2mo10/application/flask_blog/views.py
+++ ma2mo10/application/flask_blog/views.py
@@ -1,6 +1,8 @@
+from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+
 
 @app.route('/')
 # リクエストがあった時の処理  ラッパーに渡す
-def hello_world():
-    return "Hello World"
+def show_entries():
+    return render_template('entries/index.html')
