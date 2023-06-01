450e5c4b99fe394a6dddfe6518ee88d75939ed27
RyuyaAbe-dev <abe.ryuya.vo@mynavi.jp>
2023-06-01 08:25:16 +0000

    salary 追加

--- abe_flask/calcsalary_app/views.py
+++ abe_flask/calcsalary_app/views.py
@@ -9,20 +9,21 @@ def index():
 @app.route('/calc', methods=['GET', 'POST'])
 def calc():
     tax_rate = 0
+    print(request.form)
     if request.method == 'POST':
         salary = int(request.form.get('salary'))
         if (salary < 1000000):
             tax_rate = 0.1
             tax = salary*tax_rate
             payment = salary - tax
-            return render_template('output.html',payment = math.floor(payment), tax= math.floor(tax))
+            return render_template('output.html',salary = salary,payment = math.floor(payment), tax= math.floor(tax))
         else:
             tax_rate = 0.1
             tmp = salary - 1000000
             tax = 1000000 * tax_rate
             tax = tax + tmp * 0.2
             payment = salary - tax
-            return render_template('output.html',payment = math.floor(payment), tax= math.floor(tax))
+            return render_template('output.html',salary = salary,payment = math.floor(payment), tax= math.floor(tax))
             # return print(f'支給額:{math.floor(payment)}、税額:{math.floor(tax)}',end="")
 
 if __name__ == "__main__":
