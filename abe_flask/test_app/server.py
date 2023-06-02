6e25ecfd15f971149a180a8ec9a84ab06efef213
RyuyaAbe-dev <abe.ryuya.vo@mynavi.jp>
2023-06-02 00:51:47 +0000

    メール配信追加

--- abe_flask/test_app/server.py
+++ abe_flask/test_app/server.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587                             # TLSは587、SSLなら465
-app.config['MAIL_USERNAME'] = 'testttttt886@gmail.com'
-app.config['MAIL_PASSWORD'] = '@testmynaviapp'        # GmailのApp用のmパスワード設定をしておく必要あり
+app.config['MAIL_USERNAME'] = 'testmynaviflask@gmail.com'
+app.config['MAIL_PASSWORD'] = ''        # GmailのApp用のmパスワード設定をしておく必要あり
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEFAULT_SENDER'] = 'testttttt886@gmail.com'    # これがあるとsender設定が不要になる
+app.config['MAIL_DEFAULT_SENDER'] = 'testmynaviflask@gmail.com'    # これがあるとsender設定が不要になる
 mail = Mail(app)
 
 
@@ -22,7 +22,7 @@ def index():
 
 @app.route("/send_mail")
 def send_mail():
-    msg = Message('Test Mail', recipients=['ryuya00617@gmail.com'])
+    msg = Message('Test Mail', recipients=['testttttt886@gmail.com'])
     msg.body = "Hello Flask message sent from Flask-Mail"
     mail.send(msg)
     return "Sent"
