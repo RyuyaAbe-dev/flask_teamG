45c15d799a83e591990812f3e2a96eb509294829
RioTakahashi-6 <takahashi.rio.pw@mynavi.jp>
2023-06-05 02:00:45 +0000

    高橋

---
+++ Takahashi/calcsalary_app/server.py
@@ -0,0 +1,9 @@
+from flask import request, redirect, url_for, render_template, flash, session
+from flask import Flask
+from salary import app
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
